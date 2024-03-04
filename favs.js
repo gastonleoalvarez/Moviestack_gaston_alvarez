d7f348ab326770c4a88a3330de3fafc9d616f7a9
Silvia Ledesma <leo_alva97@hotmail.com>
2024-03-04 13:47:06 -0300

    hago cambios

---
+++ favs.js
@@ -0,0 +1,31 @@
+import { diseñoTarjeta } from './funciones.js';
+
+document.addEventListener('DOMContentLoaded', function () {
+    const favoritosContenedor = document.getElementById('favoritos-contenedor');
+
+    //función para mostrar las películas favoritas
+    function mostrarPeliculasFavoritas() {
+        const favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
+
+        favoritosContenedor.innerHTML = ''; //limpiar el contenedor de favoritos
+
+        //muestra las películas favoritas en el contenedor
+        favoritos.forEach(pelicula => {
+            favoritosContenedor.innerHTML += diseñoTarjeta(pelicula);
+        });
+    }
+
+    //llama a la función para mostrar las películas favoritas al cargar la página
+    mostrarPeliculasFavoritas();
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const addToFavoritesButtons = document.querySelectorAll('.addToFavoritesBtn');
+
+    addToFavoritesButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const movieId = this.dataset.movieId;
+            addToFavorites(movieId);
+        });
+    });
+});
